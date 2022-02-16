8abc023e4ea27a9b90c4a6695f688c1f8dbddff6
vadevid <kislitsun-van@mail.ru>
2022-02-16 21:50:58 +0300

    Completed prt1

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div>
     <Fio lastName="Кислицын" firstName="Иван" middleName="Александрович" />
-    <Vyatsu :results="results" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import axios from 'axios';
 import Fio from '../../fio/src/components/Fio.vue';
-import Vyatsu from '../../vyatsu/src/components/Vyatsu.vue';
-
-const url = 'http://localhost:8080/admission/';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    Fio, Vyatsu,
+    Fio,
   },
   data() {
-    return { Fio, results: [] };
-  },
-  mounted() {
-    axios.get(url).then((response: any) => {
-      console.log(response);
-      this.results = response.data;
-    }).catch((error: any) => console.log(error));
+    return { results: [] };
   },
 });
 </script>
